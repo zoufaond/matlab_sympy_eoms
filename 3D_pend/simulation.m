3c39e1933dd2872c8cee645b79634fb05375546f
zoufaond <zoufik.o@seznam.cz>
2024-01-02 21:31:04 +0100

    playing

--- 3D_pend/simulation.m
+++ 3D_pend/simulation.m
@@ -1,7 +1,7 @@
-t_end = 100;
-initialConditions_moje = [0.7,0.7,0.7,0,0,0];
+t_end = 10;
+initialConditions_moje = [1,0.5,0.4,0,0,0];
 
-
+force = 60;
 %% brick solid constants
 mass = 10.0;
 l = 5;
@@ -9,15 +9,16 @@ a = 0.5;
 b = -0.5;
 g = 9.80665;
 IB = [84.1667, 84.1667, 1.66667,0, 0, 0];
-c = [1,1,1];
+c = [1,1,1]*1;
+k = [1,1,1]*1;
 %% simulation
 
 out = sim("pend_3D_sim.slx",t_end); % run simscape first
-f = @(t, x) pend(t, x, g, l, mass, IB, a,b,c); 
+f = @(t, x) pend(t, x, g, l, mass, IB, a,b,c,k,force); 
 
 % Integrate the equations of motion with default integration settings.
 options = odeset('AbsTol',1e-3,'RelTol',1e-3);
-[t, x] = ode45(f, out.x_sim.time, initialConditions_moje); %run ODE with the same timeSpan and step as Simscape did
+[t, x] = ode89(f, out.x_sim.time, initialConditions_moje); %run ODE with the same timeSpan and step as Simscape did
 % Plot the results.
 fig = figure();
 plot(t, x(:,1),t,out.x_sim.signals.values(:))
